cd81b2aac928e3403a25a3df75ee1fe0950eda74
QuentinR <quentin.rouhier@gmail.com>
2017-02-21 11:54:41 +0100

    creation d'un fichier javascript appeler depuis le html et utilisation du modu ->le reste d'un division

---
+++ fichier.js
@@ -0,0 +1,7 @@
+function addition(){
+  var firstNumber = document.getElementById('premier_nombre').value;
+  var secondNumber = document.getElementById('deuxieme_nombre').value;
+  var result;
+  result = firstNumber % secondNumber;
+  alert(result)
+}
