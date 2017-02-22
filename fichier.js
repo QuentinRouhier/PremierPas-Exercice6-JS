7180622e0b294b1dba29c25048a8f5135dba112b
QuentinRouhier <quentin.rouhier@gmail.com>
2017-02-22 10:44:50 +0100

    Update fichier.js

--- fichier.js
+++ fichier.js
@@ -1,7 +1,11 @@
 function addition(){
   var firstNumber = document.getElementById('premier_nombre').value;
   var secondNumber = document.getElementById('deuxieme_nombre').value;
-  var result;
-  result = firstNumber % secondNumber;
-  alert(result)
-}
+   // si la valeur n'est pas un nombre tu dit : il faut des nombres
+  if (isNaN(firstNumber) || isNaN(secondNumber)){
+    alert('Il faut des nombres');
+  } else { //sinon tu me donne le resultat de la multiplication
+    var result = parseInt(firstNumber) % secondNumber;
+    alert(result);
+  }
+  };
