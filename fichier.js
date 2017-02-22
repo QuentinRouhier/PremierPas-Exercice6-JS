152895d7d26cbdeb09dacdf7a1082e7306dcef10
QuentinRouhier <quentin.rouhier@gmail.com>
2017-02-22 10:48:18 +0100

    Update fichier.js

--- fichier.js
+++ fichier.js
@@ -4,7 +4,10 @@ function addition(){
    // si la valeur n'est pas un nombre tu dit : il faut des nombres
   if (isNaN(firstNumber) || isNaN(secondNumber)){
     alert('Il faut des nombres');
-  } else { //sinon tu me donne le resultat de la multiplication
+  } else if (secondNumber==0){
+    alert('Impossible de diviser par 0');
+  }
+  else { //sinon tu me donne le resultat de la multiplication
     var result = parseInt(firstNumber) % secondNumber;
     alert(result);
   }
